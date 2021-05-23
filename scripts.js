052a6b7688bcbf9ead63dee3ba5d520448071416
praveen089942 <praveen.reddyg595@yahoo.com>
2021-05-23 12:16:20 +0530

    Inserted JS code

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const red = document.querySelector(".red");
+const cyan = document.querySelector(".cyan");
+const violet = document.querySelector(".violet");
+const orange = document.querySelector(".orange");
+const pink = document.querySelector(".pink");
+const blue = document.querySelector(".blue");
+
+const center = document.querySelector(".center");
+
+console.log(window.getComputedStyle(red).backgroundColor);
